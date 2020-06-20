257515a1065042a55347502176ed302eca60c211
Michał Jaworski <jawor403@gmail.com>
2020-06-20 23:04:32 +0200

    More flexible simulations

--- simulation.py
+++ simulation.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 from matplotlib import patches
 
 
-def vector_product(x, y):
+def _vector_product(x, y):
     return x[:, :, :, 0] * y[:, :, :, 1] - x[:, :, :, 1] * y[:, :, :, 0]
 
 
-def intersect(x1, d1, x2, d2):
+def _intersect(x1, d1, x2, d2):
     """
     Arguments:
     x1, d1: (n, d, 2)
@@ -19,28 +19,28 @@ def intersect(x1, d1, x2, d2):
     x2 = x2[None, None, :, :]
     d1 = d1[:, :, None, :]
     d2 = d2[None, None, :, :]
-    rs = vector_product(d1, d2)
-    t = vector_product(x2 - x1, d2) / rs
-    u = vector_product(x2 - x1, d1) / rs
+    rs = _vector_product(d1, d2)
+    t = _vector_product(x2 - x1, d2) / rs
+    u = _vector_product(x2 - x1, d1) / rs
     tu = np.stack([t, u], 3)
     m = np.any((tu < 0) | (tu > 1), axis=3)
     tu[m] = np.nan
     return tu
 
 
-def make_rectangle(x1, x2):
+def _make_rectangle(x1, x2):
     """
     Arguments:
     x1: lower left corner
     x2: upper right corner
     """
-    dx = np.array([x2[0] - x1[0], 0])
-    dy = np.array([0, x2[1] - x1[1]])
+    dx = np.asarray([x2[0] - x1[0], 0])
+    dy = np.asarray([0, x2[1] - x1[1]])
 
     return np.vstack([x1, x1, x2, x2]), np.vstack([dx, dy, -dx, -dy])
 
 
-def alphas_to_coords(S, x0, y0, alphas):
+def _alphas_to_coords(S, x0, y0, alphas):
     S = np.array(S)
     alphas = np.cumsum(alphas, 1)
     x = np.zeros((alphas.shape[0], alphas.shape[1] + 1, 2))
@@ -51,7 +51,7 @@ def alphas_to_coords(S, x0, y0, alphas):
     return x, d
 
 
-def rectangle_penalty(s1, x1, s2, x2, rectangle_x, rectangle_d, max_h):
+def _rectangle_penalty(s1, x1, s2, x2, rectangle_x, rectangle_d, max_h):
     S_total = rectangle_d[0, 0] * rectangle_d[1, 1]
     if s1 == s2:
         return (np.abs(np.sum(x2 - x1)) + np.abs(rectangle_d[s1%2, s1%2]) * 1e-4) * max_h / 2
@@ -67,9 +67,9 @@ def rectangle_penalty(s1, x1, s2, x2, rectangle_x, rectangle_d, max_h):
         return x[0] * x[1] / 2
 
 
-def rectangle_total_penalty(population_x, population_d, rectangle_x, rectangle_d):
+def _rectangle_total_penalty(population_x, population_d, rectangle_x, rectangle_d):
     n, d = population_d.shape[:2]
-    intersections = intersect(population_x[:, :-1, :], population_d, rectangle_x, rectangle_d)
+    intersections = _intersect(population_x[:, :-1, :], population_d, rectangle_x, rectangle_d)
     does_intersect = np.any(~np.isnan(intersections), 3)
     penalty = np.zeros(n)
     for t in range(n):
@@ -84,8 +84,8 @@ def rectangle_total_penalty(population_x, population_d, rectangle_x, rectangle_d
                 if does_intersect[t, i, j]:
                     j_point = population_x[t, i] + population_d[t, i] * intersections[t, i, j, 0]
                     if intersection_side is not None:
-                        penalty[t] += rectangle_penalty(intersection_side, intersection_point, j, j_point, rectangle_x,
-                                                        rectangle_d, max_h)
+                        penalty[t] += _rectangle_penalty(intersection_side, intersection_point, j, j_point, rectangle_x,
+                                                         rectangle_d, max_h)
                         intersection_side = None
                     else:
                         intersection_side = j
@@ -96,17 +96,34 @@ def rectangle_total_penalty(population_x, population_d, rectangle_x, rectangle_d
     return penalty
 
 
-def inverse_kinematics_objective(S, target, rectangles):
-    rectangle_segments = [make_rectangle(*rect) for rect in rectangles]
+def target_with_velocity(target, target_v, T):
     target = np.array(target)
+    target_v = np.array(target_v)
+    return [target + t * target_v for t in range(T)]
+
+
+def rectangles_with_velocity(rectangles, rectangle_vs, T):
+    result = [[] for t in range(T)]
+    for (x1, x2), v in zip(rectangles, rectangle_vs):
+        x1 = np.asarray(x1)
+        x2 = np.asarray(x2)
+        v = np.asarray(v)
+        for t in range(T):
+            result[t].append((x1 + t * v, x2 + t * v))
+    return result
+
+
+def inverse_kinematics_objective(S, target, rectangles):
+    rectangle_segments = [_make_rectangle(*rect) for rect in rectangles]
+    target = np.asarray(target)
 
     def objective(population):
-        population_x, population_d = alphas_to_coords(S, 0, 0, population)
+        population_x, population_d = _alphas_to_coords(S, 0, 0, population)
         score = np.sum((population_x[:, -1, :] - target) ** 2, 1)
         results = [score]
 
         for rectangle_x, rectangle_d in rectangle_segments:
-            rectangle_penalties = rectangle_total_penalty(population_x, population_d, rectangle_x, rectangle_d)
+            rectangle_penalties = _rectangle_total_penalty(population_x, population_d, rectangle_x, rectangle_d)
             results.append(rectangle_penalties)
 
         results = np.stack(results, 1)
@@ -116,20 +133,19 @@ def inverse_kinematics_objective(S, target, rectangles):
     return objective
 
 
-def dynamic_inverse_kinematics_objectives(S, target, target_v, rectangles, rectangle_vs):
-    rectangle_segments = [make_rectangle(*rect) for rect in rectangles]
-    rectangle_vs = [np.array(v) for v in rectangle_vs]
-    target = np.array(target)
-    target_v = np.array(target_v)
+def dynamic_inverse_kinematics_objectives(S, targets, rectangle_lists):
+    rectangle_segment_lists = [[_make_rectangle(*rect) for rect in l] for l in rectangle_lists]
+    targets = [np.array(target) for target in targets]
 
     def objective(t, population):
-        population_x, population_d = alphas_to_coords(S, 0, 0, population)
-        score = np.sum((population_x[:, -1, :] - (target + t * target_v)) ** 2, 1)
+        population_x, population_d = _alphas_to_coords(S, 0, 0, population)
+        target = targets[t]
+        rectangle_segments = rectangle_segment_lists[t]
+        score = np.sum((population_x[:, -1, :] - target) ** 2, 1)
         results = [score]
 
-        for (rectangle_x, rectangle_d), rectangle_v in zip(rectangle_segments, rectangle_vs):
-            rectangle_x = rectangle_x + t * rectangle_v
-            rectangle_penalties = rectangle_total_penalty(population_x, population_d, rectangle_x, rectangle_d)
+        for rectangle_x, rectangle_d in rectangle_segments:
+            rectangle_penalties = _rectangle_total_penalty(population_x, population_d, rectangle_x, rectangle_d)
             results.append(rectangle_penalties)
 
         results = np.stack(results, 1)
@@ -141,28 +157,25 @@ def dynamic_inverse_kinematics_objectives(S, target, target_v, rectangles, recta
 
 def draw_solutions(alphas, S, target, rectangles, xlim=None, ylim=None, **kwargs):
     fig, ax = plt.subplots(**kwargs)
-    rectangles = [make_rectangle(*rect) for rect in rectangles]
+    rectangles = [_make_rectangle(*rect) for rect in rectangles]
     _draw_solution(alphas, S, target, rectangles, ax, xlim=xlim, ylim=ylim)
 
 
-def draw_dynamic_solutions(alphas, S, target, target_v, rectangles, rectangle_vs, ts, xlim=None, ylim=None, **kwargs):
-    target = np.array(target)
-    target_v = np.array(target_v)
-    rectangles = [make_rectangle(*rect) for rect in rectangles]
-    rectangle_vs = [np.array(v) for v in rectangle_vs]
+def draw_dynamic_solutions(alphas, S, targets, rectangle_lists, ts, xlim=None, ylim=None, **kwargs):
+    rectangle_segment_lists = [[_make_rectangle(*rect) for rect in l] for l in rectangle_lists]
+    targets = [np.array(target) for target in targets]
 
     nrows = kwargs.pop("nrows", 1)
     ncols = kwargs.pop("ncols", int(np.ceil(len(ts) / nrows)))
 
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols, **kwargs)
     for i, t in enumerate(ts):
-        round_target = target + t*target_v
-        round_rectangles = [(x + t*v, d) for (x, d), v in zip(rectangles, rectangle_vs)]
-        _draw_solution(alphas[t], S, round_target, round_rectangles, axes[i // ncols, i % ncols], xlim=xlim, ylim=ylim)
+        _draw_solution(alphas[t], S, targets[t], rectangle_segment_lists[t],
+                       axes[i // ncols, i % ncols], xlim=xlim, ylim=ylim)
 
 
 def _draw_solution(alphas, S, target, rectangles, ax, xlim=None, ylim=None):
-    x, d = alphas_to_coords(S, 0, 0, alphas)
+    x, d = _alphas_to_coords(S, 0, 0, alphas)
     for rect_x, rect_d in rectangles:
         rect_patch = patches.Rectangle(rect_x[0, :], rect_d[0, 0], rect_d[1, 1], edgecolor="k", facecolor=(0, 0, 0, 0),
                                        linewidth=3)
